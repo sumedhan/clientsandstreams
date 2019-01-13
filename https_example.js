60fe1030dfc084353bc5a98987a253cb845b1723
sumedha <sumedhanarayanan@gmail.com>
2019-01-13 19:45:41 +0000

    Made the request. Step 2

--- https_example.js
+++ https_example.js
@@ -1,3 +1,19 @@
 var https = require('https');
 
-console.log("Its done.")
+console.log("Required HTTPS");
+
+var options = {
+    host: 'www.example.org',
+    path: '/'
+};
+
+//called by https when the request is made
+var callback = function() {
+    console.log('In response handler callback! wootwoot!');
+}
+
+console.log('I am about to make a request!');
+
+https.request(options, callback).end();
+
+console.log("Made the request!");
